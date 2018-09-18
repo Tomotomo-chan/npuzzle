e24e1b6ea1e4ff8ae019488b61ccc71d0c259b08
jonas <jonasbellessa@gmail.com>
2018-09-18 11:16:36 +0200

    advance in parsing

--- sources/parser.py
+++ sources/parser.py
@@ -1,24 +1,34 @@
 import argparse
 import sys
-
+import re
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-e","--heuristic", choices=["One","Two","Tree"], default="One", help="choose the heurtistic")
 parser.add_argument("-f", "--file", type=file, help="read map, if empty read stdin")
+parser.add_argument("-g", "--graphic", action="store_true", help="if True use graphic NPuzzle")
 
 args = parser.parse_args()
-
-line_num = 0
 
 if args.file:
     map = args.file
 else:
     map = sys.stdin
-for line in map:
-    if (line_num == 0):
-        if (line)
 
-    print line,
+print '\n'
 
-r = r"(-?\d+)( *#.*)?"
+def parse:
+    first = True
+    size = 0
+    puzzle
+
+
+    for line in map:
+        line_util = line.split('#')[0]
+        parse_line(line_util, first)
+
+    r = r"(\s*-?\d+)"
+
+def parse_line(line, first):
+    re 
+
