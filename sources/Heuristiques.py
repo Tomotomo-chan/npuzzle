acf11ef095d03950a8434c5afb774b8cd98ea71f
jonas <jonasbellessa@gmail.com>
2018-09-19 10:42:28 +0200

    heuritiaues class progress

--- sources/Heuristiques.py
+++ sources/Heuristiques.py
@@ -1,35 +1,71 @@
 
 from Puzzle import Puzzle
 from PuzzleGenerator import puzzle_generator
+from PuzzleCompare import puzzle_compare
 
 class Heuristiques:
 
-    default_puzzle = None
 
-    def manhatan(self, puzzle):
-        if Heuristiques.default_puzzle == None or Heuristiques.default_puzzle.size != puzzle.size:
-            Heuristiques.default_puzzle = puzzle_generator.generate_puzzle(puzzle.size)
+    def __init__(self):
+        self.current_heuristique = ""
+        self.default_puzzle = None
+        self.default_is_before_table = []
+
+    """ PRIVATE METHODS """
+
+    def create_before_table(self, puzzle):
+        ret = []
+        for i in range(puzzle.size**2):
+            for j in range(i, puzzle.size**2):
+                if i != j:
+                    ret.append(
+                        self.is_before(
+                            puzzle.get_piece_index(i),
+                            puzzle.get_piece_index(j)
+                        )
+                    )
+        return ret
+
+    def is_before(self,index1, index2):
+        if index1.line == index2.line:
+            return index1.column < index2.column
+        return index1.line < index2.line
+
+
+    def check_default(self , puzzle):
+        if self.default_puzzle == None or self.default_puzzle.size != puzzle.size:
+            self.default_puzzle = puzzle_generator.generate_puzzle(puzzle.size)
+        if self.current_heuristique == HeuristiquesType.melange and len(self.default_is_before_table) == 0:
+            self.default_is_before_table = self.create_before_table(self.default_puzzle)
+
+    def change_heuristique(self, heuristique):
+        if self.current_heuristique is not heuristique:
+            self.current_heuristique = heuristique
+            self.default_is_before_table = []
+
+    """ PUBLIC METHODS """
+
+    def manhattan(self, puzzle):
+        self.change_heuristique(HeuristiquesType.manhattan)
+        self.check_default(puzzle)
         tot = 0
-        for i in range(0,puzzle.size * puzzle.size ):
-            start = puzzle.get_piece_index(i)
-            end = Heuristiques.default_puzzle.get_piece_index(i)
-            dist = abs(start.line - end.line) + abs(start.column - end.column)
+        for i in range(puzzle.size * puzzle.size):
+            dist = puzzle_compare.nb_move_needed_for_value(i, self.default_puzzle, puzzle)
             tot += dist
-            print "for " + str(i) + " : " + str(dist)
-
-        print '\n'
-        print str(puzzle)
-        print '\n'
-        print str(Heuristiques.default_puzzle)
-
         return tot
 
-    def melange(puzzle):
-        if default_puzzle == None:
-            default_puzzle = puzzle_generator.generate_puzzle(puzzle.size)
-        return
+    def melange(self, puzzle):
+        self.change_heuristique(HeuristiquesType.melange)
+        self.check_default(puzzle)
 
+        return is_before_table
+    
+class HeuristiquesType:
+    manhattan = "manhattan"
+    melange = "melange"
 
 heuristiques = Heuristiques()
-polo = Puzzle([[1,2,3],[4,5,6],[7,8,0]])
-print "distances totales : " + str(heuristiques.manhatan(polo))
+
+polo = puzzle_generator.generate_random_puzzle(3)
+print polo
+print str(heuristiques.manhattan(polo))
